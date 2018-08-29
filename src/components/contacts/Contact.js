33eec23c6b59db98db23e3a8dabd756c728dbcae
grigormargaryan <margaryanmgrigor@gmail.com>
2018-08-29 15:29:41 +0400

    sdfsd

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -17,6 +17,7 @@ class Contact extends Component {
   onDeleteClick = async (id,dispatch) => {
     try{
       await axios.delete(`http://jsonplaceholder.typicode.com/users/${id}`)
+      dispatch({type: 'DELETE_CONTACT',payload: id});
     } catch (e) {
       dispatch({type: 'DELETE_CONTACT',payload: id});
     }
